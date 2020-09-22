57cdd266cb48a90040aada5d19cef39a6ace194f
Ji Hye <fearnonemyself@gmail.com>
2020-09-22 21:49:02 +0900

    modify index js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 $(function() {
-    // 모바일 화면에서 햄버거 메뉴 클릭 시 애니메이션
+    // 모바일 화면에서 햄버거 메뉴 클릭 시 내비게이션이 나타나는 애니메이션
     var wrapperMenu = document.querySelector('.wrapper-menu');
 
     wrapperMenu.addEventListener('click', function(){
         wrapperMenu.classList.toggle('open');
+        $('nav').animate({left:'0%'}, 500).toggleClass('show');
     })
 });
