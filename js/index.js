b44577e50b73aeef4f9dba04eb7c0e67c0f95f4d
Ji Hye <fearnonemyself@gmail.com>
2020-09-23 14:54:28 +0900

    modify file

--- js/index.js
+++ js/index.js
@@ -3,7 +3,25 @@ $(function() {
     var wrapperMenu = document.querySelector('.wrapper-menu');
 
     wrapperMenu.addEventListener('click', function(){
-        wrapperMenu.classList.toggle('open');
-        $('nav').animate({left:'0%'}, 500).toggleClass('show');
+        const style = $('header').hasClass('active');
+
+        if(style == 0) {
+            wrapperMenu.classList.add('open');
+            $('header').addClass('active');
+            $('header h1').addClass('active');
+            $('header .search').addClass('active');
+            $('nav').animate({left:'0%'}, 1000, 'easeOutCubic').addClass('show');
+        } else {
+            wrapperMenu.classList.remove('open');
+            $('header').removeClass('active');
+            $('header h1').removeClass('active');
+            $('header .search').removeClass('active');
+            $('nav').animate({left:'-100%'}, 1000, 'easeOutCubic').removeClass('show');
+        }
     })
+
+    // #scroll_btn 클릭 시 화면 제일 위로 가기
+    $('#scroll_btn').on('click', function() {
+        $('html, body').animate({scrollTop:0}, 1000);
+   });
 });
