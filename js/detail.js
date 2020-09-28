fae2683c83967dbd66b613bde91ebf89f15191bd
Ji Hye <fearnonemyself@gmail.com>
2020-09-28 17:22:37 +0900

    modify JS/jQuery

--- js/detail.js
+++ js/detail.js
@@ -72,18 +72,24 @@ $(function() {
        }
    });
 
-   // 스크롤 시 숨어 있던 #scroll_btn 보임
+   // 모든 디바이스에서 스크롤 시 숨어 있던 #scroll_btn, 모바일, 태블릿 화면에서 구매하기 버튼 보임
    $(window).scroll(function () {
         var wt = $(window).width();
         if (wt > 0 && wt < 1440) {
             var scrollT = $(window).scrollTop();
-            if (scrollT >= 100) {
+            if (scrollT >= 1000) {
                 $('#scroll_btn').stop().animate({
                     bottom: 65
+                }, 600);
+                $('#buy').stop().animate({
+                    bottom: 0
                 }, 600);
             } else {
                 $('#scroll_btn').stop().animate({
                     bottom: -175
+                }, 600);
+                $('#buy').stop().animate({
+                    bottom: -50
                 }, 600);
             }
         } else {
